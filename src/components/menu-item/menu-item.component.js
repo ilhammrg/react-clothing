86ee522a55d12ed5e234c1edebec870493dd52f8
Ilham Muhammad <ilhammrg@gmail.com>
2020-06-18 16:00:31 +0700

    update menu-item

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './menu-item.style.scss';
 
-const MenuItem = ({title}) => {
+const MenuItem = ({ title, imageUrl, size }) => {
     return (
-        <div className='menu-item'>
+        <div 
+            className={`menu-item ${size}`}
+            style={
+                {backgroundImage: `url(${imageUrl})`}
+            }
+        >
             <div className='content'>
                 <h1 className='title'>{title}</h1>
                 <span className='subtitle'>SHOW NOW</span>
