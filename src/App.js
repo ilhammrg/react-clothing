8f1c474a821d3f0c9a6949eef692b3b8d6ba9c84
Ilham Muhammad <ilhammrg@gmail.com>
2020-06-18 17:17:55 +0700

    Add menu-item style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 
 function App() {
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -3,14 +3,15 @@ import './menu-item.style.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => {
     return (
-        <div 
-            className={`menu-item ${size}`}
-            style={
-                {backgroundImage: `url(${imageUrl})`}
-            }
-        >
+        <div className={`${size} menu-item`} >
+            <div 
+                className='background-image' 
+                style={
+                    {backgroundImage: `url(${imageUrl})`}
+                }
+            />
             <div className='content'>
-                <h1 className='title'>{title}</h1>
+                <h1 className='title'>{title.toUpperCase()}</h1>
                 <span className='subtitle'>SHOW NOW</span>
             </div>
         </div>
