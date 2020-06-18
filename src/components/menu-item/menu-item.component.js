34e9d6e99ce9c1dbfe11cdfdfaea4d0f51489fd1
Ilham Muhammad <ilhammrg@gmail.com>
2020-06-18 15:45:04 +0700

    Add directory-menu component

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './menu-item.style.scss';
 
 const MenuItem = ({title}) => {
     return (
--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,41 +1,11 @@
 import React from 'react';
+import DirectoryMenu from '../../components/directory-menu/directory-menu.component';
 import './homepage.styles.scss';
 
 const Homepage = () => {
     return (
         <div className='homepage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>HATS</h1>
-                        <span className='subtitle'>SHOW NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>JACKETS</h1>
-                        <span className='subtitle'>SHOW NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>SNEAKERS</h1>
-                        <span className='subtitle'>SHOW NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>WOMENS</h1>
-                        <span className='subtitle'>SHOW NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>MENS</h1>
-                        <span className='subtitle'>SHOW NOW</span>
-                    </div>
-                </div>
-            </div>
+            <DirectoryMenu />
         </div>
     )
 }
