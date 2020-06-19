965bf96b7aeea27560e1b02ba85f30c09271c18d
Ilham Muhammad <ilhammrg@gmail.com>
2020-06-19 14:54:20 +0700

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Link to='/homepage'>Homepage</Link>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Route exact path='/' component={HelloWorld} />
+      <Route path='/homepage' component={Homepage} />
     </div>
   );
 }
