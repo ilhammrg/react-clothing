eccf8f6607f5a635bf4a0353428d483cbb90d843
Ilham Muhammad <ilhammrg@gmail.com>
2020-08-05 15:54:18 +0700

    Redirect user to home when signin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 // Redux
 import { connect } from 'react-redux';
@@ -48,11 +48,21 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path='/' component={Homepage} />
-          <Route path='/signin' component={SigninAndSignup} />
+          <Route 
+            exact 
+            path='/signin' 
+            render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SigninAndSignup />)} 
+          />
           <Route path='/shop' component={Shop} />
         </Switch>
       </div>
     );
+  }
+}
+
+const mapStateToProps = ({ user }) => {
+  return {
+    currentUser: user.currentUser
   }
 }
 
@@ -63,4 +73,4 @@ const mapDispatchToProps = dispatch => {
 }
 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
