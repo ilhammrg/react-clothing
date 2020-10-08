3704e1015dce908df7396abf1f550cb797022374
Ilham Muhammad <ilhammrg@gmail.com>
2020-10-08 11:06:36 +0700

    add stripe alert when payment failure

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -4,39 +4,42 @@ import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
 
 const StripeButton = ({ price }) => {
-    const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_51HHP39A5b3dXVUCuVpd00QDF5aPDPZiDn13DGKjxaeNRWaAjXG4O3wWDTQYh7oCGQjYKroMPotU1AHZH2EEcZvGd00voKuCKdd';
-    const onToken = token => {
-        axios({
-            url: 'payment',
-            method: 'post',
-            data: {
-                amount: priceForStripe,
-                token
-            }
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HHP39A5b3dXVUCuVpd00QDF5aPDPZiDn13DGKjxaeNRWaAjXG4O3wWDTQYh7oCGQjYKroMPotU1AHZH2EEcZvGd00voKuCKdd';
+  const onToken = token => {
+    if (priceForStripe !== 0) {
+      axios({
+        url: 'payment',
+        method: 'post',
+        data: {
+            amount: priceForStripe,
+            token
+        }
+      })
+        .then(response => {
+          alert('Payment success!');
         })
-            .then(response => {
-                alert('Payment success!');
-            })
-            .catch(error => {
-                console.log('Payment error:', JSON.parse(error));
-                alert('There was an issue with your payment. Please make sure you use the provided credit card');
-            });
-        };
+        .catch(error => {
+          console.log('Payment error:', JSON.parse(error));
+          alert('There was an issue with your payment. Please make sure you use the provided credit card');
+        });
+    }
+    alert('Cannot proceed payment! Your cart is empty!');
+  };
 
-    return (
-        <StripeCheckout 
-            name='Urban Clothing'
-            description='Please fill your payment information'
-            image='./triangle.svg'
-            panelLabel='Pay Now'
-            shippingAddress
-            billingAddress
-            amount={priceForStripe}
-            stripeKey={publishableKey}
-            token={onToken}
-        />
-    );
+  return (
+    <StripeCheckout 
+      name='Urban Clothing'
+      description='Please fill your payment information'
+      image='./triangle.svg'
+      panelLabel='Pay Now'
+      shippingAddress
+      billingAddress
+      amount={priceForStripe}
+      stripeKey={publishableKey}
+      token={onToken}
+    />
+  );
 };
 
 export default StripeButton;
